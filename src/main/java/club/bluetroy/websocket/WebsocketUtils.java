4c4aecc5f62331691aa069bc3ac7168216d54dbc
heyixin <764118015@qq.com>
2019-05-16 13:35:58 +0800

    修复websocketUtils无法编译的问题

--- src/main/java/club/bluetroy/websocket/WebsocketUtils.java
+++ src/main/java/club/bluetroy/websocket/WebsocketUtils.java
@@ -1,10 +1,13 @@
 package club.bluetroy.websocket;
 
+import club.bluetroy.http.HttpRequest;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.Map;
 
 /**
  * @author heyixin 2019-05-16 13:08.
@@ -21,8 +24,7 @@ import java.util.Base64;
  * Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=
  */
 public class WebsocketUtils {
-    private static void handle(String http, OutputStream out) throws IOException, NoSuchAlgorithmException {
-        Websocket.HttpRequest httpRequest = parseHttpRequest(http);
+    private static void handle(HttpRequest httpRequest, OutputStream out) throws IOException, NoSuchAlgorithmException {
         if (isWebsocketUpgrade(httpRequest)) {
             String response = "HTTP/1.1 101 Switching Protocols\r\n" +
                     "    Upgrade: websocket\r\n" +
@@ -34,11 +36,13 @@ public class WebsocketUtils {
         }
     }
 
-    private static boolean isWebsocketUpgrade(Websocket.HttpRequest httpRequest) {
-        return "websocket".equals(httpRequest.head.get("Upgrade")) && "Upgrade".equals(httpRequest.head.get("Connection"));
+    private static boolean isWebsocketUpgrade(HttpRequest httpRequest) {
+        Map<String, String> head = httpRequest.getHead();
+        return "websocket".equals(head.get("Upgrade")) && "Upgrade".equals(head.get("Connection"));
     }
 
-    private static String getSec(Websocket.HttpRequest httpRequest) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        return Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1").digest((httpRequest.head.get("Sec-WebSocket-Key") + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes(StandardCharsets.UTF_8)));
+    private static String getSec(HttpRequest httpRequest) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        Map<String, String> head = httpRequest.getHead();
+        return Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1").digest((head.get("Sec-WebSocket-Key") + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes(StandardCharsets.UTF_8)));
     }
 }
